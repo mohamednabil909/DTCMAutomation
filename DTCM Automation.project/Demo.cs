2a6a17e31f9c451413e5cf27a88c7d0be734124d
Mohamed El katry <Mohamed.Elkatry@linkdev.com>
2020-03-22 03:56:33 +0200

    Add project files.

---
+++ DTCM Automation.project/Demo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using Microsoft.Dynamics365.UIAutomation.Api;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using System.Security;
+using System.Threading;
+
+namespace DTCM_Automation.project
+{
+    [TestClass]
+    public class Demo
+    {
+
+        private readonly SecureString _username = Properties.Settings.Default.OnlineUsername.ToSecureString(); //["OnlineUsername"].ToSecureString();
+        private readonly SecureString _password = Properties.Settings.Default.OnlinePassword.ToSecureString(); //["OnlinePassword"].ToSecureString();
+        private readonly Uri _xrmUri = new Uri(Properties.Settings.Default.OnlineCrmUrl.ToString()); //["OnlineCrmUrl"].ToString());
+
+        [TestMethod]
+        public void Login()
+        {
+            
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                xrmBrowser.Driver.WaitForPageToLoad();
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+                Thread.Sleep(5000);
+                xrmBrowser.Dialogs.CloseWarningDialog(2);
+                xrmBrowser.ThinkTime(500);
+                xrmBrowser.Navigation.OpenSubArea("Profile Managment", "Accounts");
+
+            }
+
+        }
+    }
+}
---
+++ DTCM Automation.project/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DTCM Automation.project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DTCM Automation.project")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e1e60d7d-6904-4312-81e2-29946e78b8a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
