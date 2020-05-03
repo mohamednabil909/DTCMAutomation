9d7224cefee7fc5ad76121a687953925d00657be
Mariana Rezkalla <Mariana.Rezkalla@linkdev.com>
2020-05-03 21:34:38 +0200

    Festival test cases update

---
+++ DTCM Automation.project/TestCases/FestivalParticipationTestCases.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DTCM_Automation.project.CommonFunctions;
+using DTCM_Automation.project.Steps;
+using static DTCM_Automation.project.CommonFunctions.CommonFunctions;
+using static DTCM_Automation.project.CommonFunctions.Enums;
+using Microsoft.Dynamics365.UIAutomation.Api;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+
+namespace DTCM_Automation.project.TestCases
+{
+    /// <summary>
+    /// Summary description for Festival
+    /// </summary>
+    [TestClass]
+    public class FestivalParticipationTestCases
+    {
+        PortalFormsClass portalForms = new PortalFormsClass();
+        CRMSteps CRMSteps = new CRMSteps();
+        CommonFunctions.CommonFunctions CommonFunctions = new CommonFunctions.CommonFunctions();
+        string guid, RequestId;
+        /* Initialize Runs at the Start of Run/Debug of Each Test Method
+     * Opens New Driver and Initializes its Wait
+     */
+        [TestInitialize]
+        public void Portal_Initialize()
+        {
+            portalForms.Intialize();
+        }
+
+        /* Cleanup Runs at the End of Run/Debug of Each Test Method
+         * Closes Open Driver
+         */
+
+        [TestCleanup]
+        public void Portal_CleanUp()
+        {
+            portalForms.CloseDriver();
+        }
+
+        // Add promotion of type "Discount"
+        [TestMethod]
+        public void TC_FestivlRequest_AddDiscountfromportal()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+    
+            portalForms.FestivalParticipationRequest_DetailsStep(Properties.Settings.Default.CompanyName,Properties.Settings.Default.Event,Promotions.Discount);
+
+            portalForms.FestivalParticipationRequest_SelectBransAndBranches(Participationselection.Brands);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.Discount);
+
+            portalForms.FestivalAttachmentsStep();
+            
+            string requestid=portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+            
+        }
+        
+        // Add promotion of type "Sale"
+        [TestMethod]
+        public void TC_FestivlRequest_AddSaleFromportal()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+            portalForms.FestivalParticipationRequest_DetailsStep(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+
+            portalForms.FestivalParticipationRequest_SelectBransAndBranches(Participationselection.Brands);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.Sale);
+
+            portalForms.FestivalAttachmentsStep();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+        }
+
+        // Add promotion of type "PartSale"
+        [TestMethod]
+        public void TC_FestivlRequest_AddPartSaleFromportal()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+            
+            portalForms.FestivalParticipationRequest_DetailsStep(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+
+            portalForms.FestivalParticipationRequest_SelectBransAndBranches(Participationselection.Brands);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.PartSale);
+
+            portalForms.FestivalAttachmentsStep();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+            
+        }
+
+       // Add promotion of type "Offer"
+        [TestMethod]
+        public void TC_FestivlRequest_AddOfferfromportal_RetailerApproveFromCRM()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+
+            portalForms.FestivalParticipationRequest_DetailsStep(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+
+            portalForms.FestivalParticipationRequest_SelectBransAndBranches(Participationselection.Brands);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.Offer);
+
+            portalForms.FestivalAttachmentsStep();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                CRMSteps.EventFirstDecisionStep(xrmBrowser, Users.Retailer, true, true, true, RequestId, Decisions.Approve);
+            }
+        }
+
+        [TestMethod]
+        public void TC_FestivlRequest_AddOfferfromportal_RetailerSendbackFromCRM()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+
+            portalForms.FestivalParticipationRequest_DetailsStep(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+
+            portalForms.FestivalParticipationRequest_SelectBransAndBranches(Participationselection.Brands);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.Offer);
+
+            portalForms.FestivalAttachmentsStep();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                CRMSteps.EventFirstDecisionStep(xrmBrowser, Users.Retailer, true, true, true, RequestId, Decisions.Sendback);
+            }
+        }
+
+        [TestMethod]
+        public void TC_FestivlRequest_AddOfferfromportal_RetailerCancelFromCRM()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+
+            portalForms.FestivalParticipationRequest_DetailsStep(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+
+            portalForms.FestivalParticipationRequest_SelectBransAndBranches(Participationselection.Brands);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.Offer);
+
+            portalForms.FestivalAttachmentsStep();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                CRMSteps.EventFirstDecisionStep(xrmBrowser, Users.Retailer, true, true, true, RequestId, Decisions.Cancel);
+            }
+        }
+
+        
+        //Add promotion of type "Kiosk"
+        [TestMethod]
+        public void TC_FestivlRequest_AddKioskfromportal_RetailerApproveFromCRM()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+
+            portalForms.FestivalParticipationRequest_DetailsStepkiosk(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+            
+            portalForms.FestivalParticipationAddPromotions(Promotions.Kiosk);
+
+            portalForms.FestivalAttachmentsStepKiosk();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                CRMSteps.EventFirstDecisionStep(xrmBrowser, Users.Retailer, true, true, true, RequestId, Decisions.Approve);
+            }
+        }
+        
+        [TestMethod]
+        public void TC_FestivlRequest_AddKioskfromportal_RetailerSendbackFromCRM()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+
+            portalForms.FestivalParticipationRequest_DetailsStepkiosk(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.Kiosk);
+
+            portalForms.FestivalAttachmentsStepKiosk();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                CRMSteps.EventFirstDecisionStep(xrmBrowser, Users.Retailer, true, true, true, RequestId, Decisions.Sendback);
+            }
+        }
+        
+        [TestMethod]
+        public void TC_FestivlRequest_AddKioskfromportal_RetailerCancelFromCRM()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+
+            portalForms.FestivalParticipationRequest_DetailsStepkiosk(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.Kiosk);
+
+            portalForms.FestivalAttachmentsStepKiosk();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                CRMSteps.EventFirstDecisionStep(xrmBrowser, Users.Retailer, true, true, true, RequestId, Decisions.Cancel);
+            }
+        }
+
+
+        //Add promotion of type "Raffle"
+        [TestMethod]
+        public void TC_FestivlRequest_AddRafflefromportal_RetailerApproveFromCRM()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+
+            portalForms.FestivalParticipationRequest_DetailsStepRaffle(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.Raffle);
+
+            portalForms.FestivalAttachmentsStep();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                CRMSteps.EventFirstDecisionStep(xrmBrowser, Users.Retailer, true, true, true, RequestId, Decisions.Approve);
+            }
+        }
+
+        [TestMethod]
+        public void TC_FestivlRequest_AddRafflefromportal_RetailerSendbackFromCRM()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+
+            portalForms.FestivalParticipationRequest_DetailsStepRaffle(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+
+            portalForms.ActivationParticipationAddpromotins(Promotions.Raffle);
+
+            portalForms.FestivalAttachmentsStep();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                CRMSteps.EventFirstDecisionStep(xrmBrowser, Users.Retailer, true, true, true, RequestId, Decisions.Sendback);
+            }
+        }
+
+        [TestMethod]
+        public void TC_FestivlRequest_AddRafflefromportal_RetailerCancelFromCRM()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+
+            portalForms.FestivalParticipationRequest_DetailsStepRaffle(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.Raffle);
+
+            portalForms.FestivalAttachmentsStep();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                CRMSteps.EventFirstDecisionStep(xrmBrowser, Users.Retailer, true, true, true, RequestId, Decisions.Cancel);
+            }
+        }
+
+
+        //Add promotion of type "Scratch and win"
+        [TestMethod]
+        public void TC_FestivlRequest_AddScratchandwinfromportal_RetailerApproveFromCRM()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+
+            portalForms.FestivalParticipationRequest_DetailsStepScratchandwin(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.Scratchandwin);
+
+            portalForms.FestivalAttachmentsStep();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                CRMSteps.EventFirstDecisionStep(xrmBrowser, Users.Retailer, true, true, true, RequestId, Decisions.Approve);
+            }
+        }
+
+
+        [TestMethod]
+        public void TC_FestivlRequest_AddScratchandwinfromportal_RetailerSendbackFromCRM()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+
+            portalForms.FestivalParticipationRequest_DetailsStepScratchandwin(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.Scratchandwin);
+
+            portalForms.FestivalAttachmentsStep();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                CRMSteps.EventFirstDecisionStep(xrmBrowser, Users.Retailer, true, true, true, RequestId, Decisions.Sendback);
+            }
+        }
+
+
+        [TestMethod]
+        public void TC_FestivlRequest_AddScratchandwinfromportal_RetailerCancelFromCRM()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+
+            portalForms.FestivalParticipationRequest_DetailsStepScratchandwin(Properties.Settings.Default.CompanyName, Properties.Settings.Default.Event);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.Scratchandwin);
+
+            portalForms.FestivalAttachmentsStep();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                CRMSteps.EventFirstDecisionStep(xrmBrowser, Users.Retailer, true, true, true, RequestId, Decisions.Cancel);
+            }
+        }
+
+        //Add promotion of type "Scratch and win" with Sponsor company
+        [TestMethod]
+        public void TC_FestivlRequest_AddScratchandwinfromportal_SponsorComapny_RetailerApproveFromCRM()
+        {
+            portalForms.Portal_LoginAndNavigateTo(ServiceName.initiativeparticipationrequest);
+
+            portalForms.FestivalParticipationRequest_DetailsStepScratchandwin(Properties.Settings.Default.CompanySponsor, Properties.Settings.Default.Event);
+
+            portalForms.FestivalParticipationAddPromotions(Promotions.Scratchandwin);
+
+            portalForms.FestivalAttachmentsStep();
+
+            string requestid = portalForms.FestivalParticipationRequest_PaymentDetailsStep();
+
+            using (var xrmBrowser = new Browser(TestSettings.Options))
+            {
+                CRMSteps.EventFirstDecisionStep(xrmBrowser, Users.Retailer, true, true, true, RequestId, Decisions.Approve);
+            }
+        }
+    }
+}
